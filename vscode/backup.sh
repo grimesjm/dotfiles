072baf6ca30b82872439780af33b6ed0d18c346a
Justin Grimes <justin.mgrimes@gmail.com>
2019-01-19 13:48:19 -0500

    Dotbot setup, fixed up vscode, fixed up sane defaults timezone

---
+++ vscode/backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp  ~/Library/Application\ Support/Code/User/keybindings.json ~/.dotfiles/vscode/keybindings.json
+cp  ~/Library/Application\ Support/Code/User/settings.json ~/.dotfiles/vscode/settings.json
