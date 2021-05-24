55dfef043481f570f091939f61caede5876eb911
Justin Grimes <justin.mgrimes@gmail.com>
2021-05-24 13:43:04 -0400

    Generate token for reader endpoints

--- zsh/aws/functions.zsh
+++ zsh/aws/functions.zsh
@@ -13,3 +13,20 @@ awsokta () {
 	eval $(aws-okta env $1 -t 8h)
 }
 
+
+awsRDSReadOnlyConnect () {
+  export RDS_WRITER_ENDPOINT=$(aws rds describe-db-cluster-endpoints  --query "DBClusterEndpoints[?contains(DBClusterIdentifier,\`$2\`) && EndpointType == \`WRITER\` && !contains(DBClusterIdentifier, \`replica\`) ]".Endpoint --output text)
+  export RDS_READER_ENDPOINT=$(aws rds describe-db-cluster-endpoints  --query "DBClusterEndpoints[?contains(DBClusterIdentifier,\`$2\`) && EndpointType ==\`READER\` && !contains(DBClusterIdentifier, \`replica\`) ]".Endpoint --output text)
+  export TOKEN="$(aws rds generate-db-auth-token --hostname $RDS_READER_ENDPOINT --port 3306 --region us-east-1 --username $1 --output text)"
+  echo "The reader endpoint the token is generated for is: $RDS_READER_ENDPOINT"
+  echo "The token:  $TOKEN"
+  echo ""
+  echo "--------------------------------------------------------------------------------------------------------------------------------"
+  echo "--------------------------------------------------------------------------------------------------------------------------------"
+  echo "--------------------------------------------------------------------------------------------------------------------------------"
+  echo "---------------------------------THIS WAS GENERATED IN $AWS_OKTA_PROFILE---------------------------------------------------------"
+  echo "--------------------------------------------------------------------------------------------------------------------------------"
+  echo "--------------------------------------------------------------------------------------------------------------------------------"
+  echo "--------------------------------------------------------------------------------------------------------------------------------"
+
+}
