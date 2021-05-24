66c3e19969e8465ab019234a51d4b4e5316986c0
Justin Grimes <justin.mgrimes@gmail.com>
2021-05-24 12:38:56 -0400

    k8s aliasess, tired of typing kubectl all the time

--- zsh/k8s/aliases.zsh
+++ zsh/k8s/aliases.zsh
@@ -1,1 +1,8 @@
-alias kubelogs=stern
+alias klogs=stern
+alias knd="kubens dev"
+alias knp="kubens prod"
+alias kns="kubens stg"
+alias kni="kubens int"
+alias kgp="kubectl get pods"
+alias kg="kubectl get"
+alias kd="kubectl describe"
